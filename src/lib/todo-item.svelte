cb5358a8c62e191f58d13fa2301620780c9f8894
lord <marialindananono@gmail.com>
2023-02-17 15:50:51 +0300

    develop html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,14 @@
+<div>
+    <form action="">
+        <input type="hidden" name='done' value=''>
+    <button aria-label="Mark done/not done">Done</button>
+    </form>
+    <form action="">
+        <input type="hidden">
+    <button aria-label="Save Todo">Done</button>
+    </form>
+    <form action="">
+        <input type="hidden">
+    <button aria-label="Delete todo">Done</button>
+    </form>
+</div>
